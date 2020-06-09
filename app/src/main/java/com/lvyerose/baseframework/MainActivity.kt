77578676bd351adb3befa2967fb91caa8054176e
lvyeRose <7758258aA>
2020-06-09 16:16:53 +0800

    添加测试适配器基本页面

--- app/src/main/java/com/lvyerose/baseframework/MainActivity.kt
+++ app/src/main/java/com/lvyerose/baseframework/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.lvyerose.baseframework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lvyerose.baseframework.recycler.RecyclerMainActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_test_recycler.setOnClickListener { startActivity(Intent(this, RecyclerMainActivity::class.java)) }
     }
+
+
 }
---
+++ app/src/main/java/com/lvyerose/baseframework/recycler/RecyclerMainActivity.kt
@@ -0,0 +1,17 @@
+package com.lvyerose.baseframework.recycler
+
+import android.os.Bundle
+import com.lvyerose.baseframework.R
+import com.lvyerose.framework.base.general.BaseActivity
+
+/**
+ * 列表适配器测试首页
+ */
+class RecyclerMainActivity : BaseActivity() {
+
+    override fun setContentLayoutId() = R.layout.activity_recycler_main
+
+    override fun startAction(savedInstanceState: Bundle?) {
+
+    }
+}
