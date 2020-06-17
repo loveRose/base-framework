596c19513a5002d927b8ff80d0deea6588ab0ed6
lvyeRose <7758258aA>
2020-06-17 18:09:36 +0800

    创建BaseRecyclerAdapter说明文档

---
+++ base/src/main/java/com/lvyerose/framework/base/mvp/BaseMvpFragment.kt
@@ -0,0 +1,10 @@
+package com.lvyerose.framework.base.mvp
+
+import com.lvyerose.framework.base.general.BaseFragment
+
+/**
+ * 使用比较少，暂时就不设计了
+ */
+abstract class BaseMvpFragment : BaseFragment() {
+
+}
